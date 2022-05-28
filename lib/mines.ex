c53b729b22deda169d26f520867b64681dff2669
goldsami <zolotarenkomax@gmail.com>
2022-05-28 20:45:55 +0300

    feat: left_click and generate_field functions creation started

--- lib/mines.ex
+++ lib/mines.ex
@@ -4,17 +4,40 @@ defmodule Mines do
   """
 
   @doc """
-  Hello world.
+  Generates field with mines
 
   ## Examples
-
-      iex> Mines.hello()
-      "ggwp"
-
+      iex> Mines.generate_field(%Coordinate{x: 1, y: 1}, %GameSettings{board_size: 3, mines_quantity: 3})
+      [
+        {1, 1}, {1, 2}, {1, 3},
+        {2, 1}, {2, 2}, {2, 3},
+        {3, 1}, {3, 2}, {3, 3}
+      ]
   """
-  def hello do
-    Test.test()
+  def generate_field(_ignore_field_coords, game_settings) do
+    cells =
+      for x <- 1..game_settings.mines_quantity,
+          do: for(y <- 1..game_settings.mines_quantity, do: {x, y})
+
+    List.flatten(cells)
   end
 
-  # def
+  @doc """
+  Left click on field. Coordinates should be in range from 1 to @board_size
+
+  ## Examples
+      iex> Mines.left_click(2, 3)
+      {:ok, "Result."}
+
+      iex> Mines.left_click(1, 44)
+      {:err, "Invalid input."}
+
+      iex> Mines.left_click(11, 4)
+      {:err, "Invalid input."}
+  """
+  def left_click(x, y) when x in 1..8 and y in 1..8 do
+    {:ok, "Result."}
+  end
+
+  def left_click(_, _), do: {:err, "Invalid input."}
 end
