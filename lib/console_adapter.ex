ca33c4df78ee926ceac31815d960e1f34e739b17
goldsami <zolotarenkomax@gmail.com>
2022-06-29 20:37:55 +0300

    feat: start game changed

--- lib/console_adapter.ex
+++ lib/console_adapter.ex
@@ -9,11 +9,29 @@ defmodule MinesConsoleGame do
     Mines.init_game(game_settings)
     |> print_game_field(game_settings)
 
+    request_valid_x_y()
+    |> Mines.start_game()
+    |> print_game_field(game_settings)
+  end
+
+  defp request_valid_x_y() do
+    request_x_y()
+    |> Mines.validate_coord()
+    |> case do
+      {:ok, coord} ->
+        coord
+
+      {:err, msg} ->
+        IO.write("\n#{msg}\n")
+        request_valid_x_y()
+    end
+  end
+
+  defp request_x_y() do
     {x, _} = IO.gets("enter x: ") |> Integer.parse()
     {y, _} = IO.gets("enter y: ") |> Integer.parse()
 
-    Mines.start_game(%Coordinate{x: x, y: y})
-    |> print_game_field(game_settings)
+    %Coordinate{x: x, y: y}
   end
 
   defp print_game_field(game_field, game_settings) do
@@ -42,6 +60,7 @@ defmodule MinesConsoleGame do
   defp write_cell_to_console(cell) do
     cond do
       cell.status == :closed -> " #"
+      cell.status == :flag -> " F"
       cell.has_mine -> " *"
       true -> " #{cell.mines_around}"
     end
